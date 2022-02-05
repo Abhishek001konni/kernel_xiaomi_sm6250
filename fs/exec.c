235fe15e837a8295fb7a0d86ac61f233e3a9d5ac
Abhishek001konni <abhishek001konni@gmail.com>
2022-02-05 07:49:23 +0530

    Revert "fs/exec: Affine fingerprint service to big cpus"
    This reverts commit 2d419ffe9c1df7b4b59b3d460e3a7dbaaa82071d.

--- fs/exec.c
+++ fs/exec.c
@@ -80,7 +80,6 @@ static DEFINE_RWLOCK(binfmt_lock);
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
-#define FP_BIN_PREFIX "/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6250"
 static struct signal_struct *zygote32_sig;
 static struct signal_struct *zygote64_sig;
 
@@ -1837,12 +1836,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
-			set_cpus_allowed_ptr(current, cpu_perf_mask);
-		}
-		else if (unlikely(!strncmp(filename->name,
-					   FP_BIN_PREFIX,
-					   strlen(FP_BIN_PREFIX)))) {
-		        current->flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
 	}
